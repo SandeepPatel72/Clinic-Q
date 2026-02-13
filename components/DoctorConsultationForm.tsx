32f823f7ed0373403148d83b9cac6680056f5310
Sandeep Patel <sandeep.patel708708@gmail.com>
2026-02-13 11:12:55 +0000

    Button Name

--- components/DoctorConsultationForm.tsx
+++ components/DoctorConsultationForm.tsx
@@ -569,7 +569,7 @@ const DoctorConsultationForm: React.FC<DoctorConsultationFormProps> = ({ patient
           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
           </svg>
-          Print & Finalize (Shift+Ent)
+          Print & Fin (Shift+Ent)
         </button>
         <button
           type="submit"
